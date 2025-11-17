b6b173a43b14ceb2dcfae7bc1cbbc1130a062aa1
Mark Altosaar <mark.altosaar@ucalgary.ca>
2025-11-16 18:35:49 -0700

    Update __init__.py

--- local_transcribe/providers/turn_builders/__init__.py
+++ local_transcribe/providers/turn_builders/__init__.py
@@ -6,3 +6,4 @@ Turn builder providers.
 # Import all turn builder modules to register them
 from . import multi_speaker_turn_builder
 from . import split_audio_turn_builder
+from . import split_audio_llm_turn_builder

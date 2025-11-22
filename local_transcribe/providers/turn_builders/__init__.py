9e3c08cfe0fd84beeb62181a7673d38d45a551c4
Mark Altosaar <mark.altosaar@ucalgary.ca>
2025-11-22 12:00:12 -0700

    Remove deprecated split_audio_llm_turn_builder

--- local_transcribe/providers/turn_builders/__init__.py
+++ local_transcribe/providers/turn_builders/__init__.py
@@ -6,6 +6,5 @@ Turn builder providers.
 # Import all turn builder modules to register them
 from . import multi_speaker_turn_builder
 from . import split_audio_turn_builder
-from . import split_audio_llm_turn_builder
 from . import split_audio_llm_turn_builder_improved
 from . import split_audio_turn_builder_improved
